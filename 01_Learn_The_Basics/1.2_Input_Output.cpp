edcb3b474bdba89aa826dc9cf03fa39302da4534
Mayur Kumawat <kumavatmayur345@gmail.com>
2026-09-18 14:03:10 +0530

    Update and rename 1.2_pattern_1.cpp to 1.2_Input_Output.cpp

---
+++ 01_Learn_The_Basics/1.2_Input_Output.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num; 
+    cin>>num; //to take number input 
+    cout<<"you number is: "<<num; // to print number or output
+    cout<<endl; // for a new line
+    string name;
+    cin>>name; // to take input of name
+    cout<<"Your name is: "<<name; // to print name or output
+}

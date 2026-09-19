3b52855bbb7a694914a767b51121799e76cbde89
Mayur Kumawat <kumavatmayur345@gmail.com>
2026-09-19 08:26:41 +0530

    Create 2.10_Pattern_10.cpp

---
+++ 02_Build_Up_Logical_Thinking/2.10_Pattern_10.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int num;
+    cin>>num;
+    for(int i=0;i<num;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=num-1;i>0;i--){
+        for(int j=0;j<i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}

b267baa1d7ef5a869afe5573bb40e67159b75d7b
Mayur Kumawat <kumavatmayur345@gmail.com>
2026-09-19 09:09:46 +0530

    Create 2.13_Pattern_13.cpp

---
+++ 02_Build_Up_Logical_Thinking/2.13_Pattern_13.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int num;
+    cin>>num;
+    int ans=1;
+    for(int i=1;i<=num;i++){
+        for(int j=1;j<=i;j++){
+            cout<<ans<<" ";
+            ans++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}

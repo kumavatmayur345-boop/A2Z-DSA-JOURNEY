fa952a06a021098b86b2848773bcd063689113ba
Mayur Kumawat <kumavatmayur345@gmail.com>
2026-09-26 16:40:44 +0530

    Create 3.17_Reverse_Array.cpp

---
+++ 03_Know_The_Basic/3.17_Reverse_Array.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> count(int arr[],int n){
+    vector<int> v;
+    for(int i=n-1;i>=0;i--){
+        v.push_back(arr[i]);
+    } return v;
+}
+int main(){
+    int a;
+    cin>>a;
+    int arr[a];
+    for(int i=0;i<a;i++){
+        cin>>arr[i];
+    }
+    vector<int> v;
+    v=count(arr,a);
+    for(int i=0;i<a;i++){
+    cout<<v[i]<<" ";
+    }
+}
